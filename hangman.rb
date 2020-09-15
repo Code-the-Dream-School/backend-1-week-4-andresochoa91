e467d721cc150a0f1fe4d245b0d6d2919c93c168
andresochoa91 <andres_ochoa91@hotmail.com>
2020-09-15 13:31:23 -0700

    ready for code review

--- hangman.rb
+++ hangman.rb
@@ -1,4 +1,4 @@
 def hangman(word, chars)
-  word.split("").inject("") { |acc, char| (chars.include? char) ? "#{acc + char}" : "#{acc}_" }
+  word.split("").inject("") { |acc, char| (chars.include? char) ? acc + char : acc + "_" }
 end
-puts hangman("justice", ["s", "a"])
+puts hangman("justice", ["s", "a", "u"])
